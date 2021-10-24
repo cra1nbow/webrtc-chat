1f0c4cb338c45da51b46f97bfa7ff53ab33fbae1
cra1nbow <dev@cra1nbow.is>
2021-10-25 08:47:32 +0900

    feat: join form

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,48 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: 100vh;
+  font-size: 1.5rem;
+`;
+
+const CodeInputText = styled.input`
+  padding: 0.25em;
+  font-size: inherit;
+`;
+
+const SubmitButton = styled.input`
+  padding: 0.25em 0.75em;
+  font-size: inherit;
+  border: none;
+  border-radius: 0.25em;
+  background-color: lightgray;
+  margin-left: 1em;
+`;
 
 const Home: NextPage = () => {
   const [code, setCode] = useState('');
+
+  const handleJoin = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setCode('');
+  };
+
   return (
-    <div>
+    <Container>
       <Head>
         <title>WebRTC chat</title>
       </Head>
-      <input value={code} onChange={(e) => setCode(e.target.value)} />
-    </div>
+      <form onSubmit={handleJoin}>
+        <CodeInputText value={code} onChange={(e) => setCode(e.target.value)} />
+        <SubmitButton type='submit' value='join' />
+      </form>
+    </Container>
   );
 };
 
